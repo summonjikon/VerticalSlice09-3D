721236620a864aadbb3fb3822988ba7a609886c0
jikon <jikon@protonmail.com>
2022-02-02 13:28:58 +0100

    update

--- Assets/JimCoins/Scripts/Coin.cs
+++ Assets/JimCoins/Scripts/Coin.cs
@@ -14,7 +14,7 @@ public class Coin : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(0,1,0);
     }
 
     private void OnTriggerEnter(Collider other)
